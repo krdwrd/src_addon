d37f9b84a3dde44776ffd4694723ada75803bc35
JSS <jss@krdwrd.org>
2008-05-30 20:48:16 +0000

    import cacert.org

--- cert/init.sh
+++ cert/init.sh
@@ -4,4 +4,8 @@
 certutil -N -d .
 
 # import keys (server certs, pub/priv keypair)
-pk12util -v -i ~/cert/krdwrd@krdwrd.org.p12 -n krdwrd -d .
+pk12util -v -i krdwrd@krdwrd.org.p12 -n krdwrd -d .
+
+# import cacert.org ca root
+wget http://www.cacert.org/certs/class3.crt
+certutil -A -i class3.crt -n cacert.org -t C,C,C -d .
