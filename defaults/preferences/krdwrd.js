8f301a91ef26be843aa17b84f05bde799e2ffcf6
Egon W. Stemle <egon@krdwrd.org>
2009-11-23 16:51:30 +0000

    refactoring - file name change; prepare some usueful prefs for may-be-later use

---
+++ defaults/preferences/krdwrd.js
@@ -0,0 +1,12 @@
+pref("javascript.enabled", false);
+
+// pref("security.warn_entering_secure", false);
+// pref("security.warn_entering_secure.show_once", false);
+// pref("security.warn_entering_weak", false);
+// pref("security.warn_entering_weak.show_once", false);
+// pref("security.warn_leaving_secure", false);
+// pref("security.warn_leaving_secure.show_once", false);
+// pref("security.warn_submit_insecure", false);
+// pref("security.warn_submit_insecure.show_once", false);
+// pref("security.warn_viewing_mixed", false);
+// pref("security.warn_viewing_mixed.show_once", false);
--- defaults/preferences/prefs.js
+++
@@ -1,1 +0,0 @@
-pref("javascript.enabled", false);
